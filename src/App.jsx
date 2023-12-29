9108458d4104e86408c4f82da5025f9ac3e56f3d
SiddharthJiyani <sidjiyani2003@gmail.com>
2023-12-29 15:22:51 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { fetchDataFromAPI } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import HomeSlice, { getApiConfig, getGenres } from "../store/homeSlice";
 import Home from "./pages/home/Home";
-import Footer from "./compenents/footer/footer";
+import Footer from "./compenents/footer/Footer";
 import Header from "./compenents/header/header";
 import PageNotFound from "./pages/404/pageNotFound";
 import Explore from "./pages/explore/explore";
